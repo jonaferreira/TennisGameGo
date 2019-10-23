417116484d20b139f9fd6d2ecd0f8990040401b1
Jonathan Ferreira <joferreira@servexternos.santandertecnologia.com.ar>
2019-10-22 21:35:05 -0300

    Upgrade calculate of scoring

--- main.go
+++ main.go
@@ -6,75 +6,71 @@ import (
 	"time"
 )
 
+// Points is ...
 var Points = [4]int{0, 15, 30, 40}
 
+// Player is ...
 type Player struct {
-	name       string
-	score      int
-	matchPoint bool
+	name      string
+	score     int
+	winner    bool
+	advantage bool
 }
 
+// Game is ....
 type Game struct {
 	player1 Player
 	player2 Player
 }
 
-func scoring(player *Player) {
-	defer player.isMatchPoint()
-	player.score++
+func (game *Game) isThereWinner() bool {
+	return !(game.player1.winner == true || game.player2.winner == true)
+
 }
 
-func (player *Player) isMatchPoint() {
-	if player.score == 3 {
-		player.matchPoint = true
-		fmt.Println("Match Point for: ", player)
+func scoring(playerWin *Player, playerLose *Player) {
+	if playerWin.score < 2 {
+		playerWin.score++
+	} else if playerWin.advantage == false && playerLose.advantage == false {
+		playerWin.advantage = true
+	} else if playerWin.advantage == true && playerLose.advantage == false {
+		fmt.Println("Winner is", playerWin.name)
+		playerWin.winner = true
+	} else if playerWin.advantage == false && playerLose.advantage == true {
+		fmt.Println("Deuse!!!")
+		playerLose.advantage = false
 	}
 }
 
 func (game *Game) wonPoint() {
 	if rand.Float64() < 0.5 {
-		scoring(&game.player1)
+		scoring(&game.player1, &game.player2)
 	} else {
-		scoring(&game.player2)
+		scoring(&game.player2, &game.player1)
 	}
-}
-
-func (game *Game) isThereDeuse() {
-	if game.player1.matchPoint && game.player2.matchPoint {
-		game.player1.matchPoint = false
-		game.player2.matchPoint = false
-		fmt.Println("Deuseee!!")
-	} else {
-		fmt.Println("There isn't deuse")
-	}
-}
-
-func (game *Game) isThereWinner() {
-
 }
 
 func (game *Game) round() bool {
 	game.wonPoint()
-	game.isThereDeuse()
-	game.isThereWinner()
-	return false
+	return game.isThereWinner()
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	game := Game{Player{name: "DelPotro", score: 2},
-		Player{name: "Djokovic", score: 3}}
+	game := Game{Player{name: "DelPotro"},
+		Player{name: "Djokovic"}}
 
 	continues := true
 	for continues {
 		continues = game.round()
-		fmt.Println(game)
+		fmt.Println(game.player1, " - ", game.player2)
 	}
 }
 
 func (player Player) String() string {
 	return fmt.Sprint("name: ", player.name,
-		" - score: ", player.score,
-		" - isYourMatchPoint: ", player.matchPoint)
+		" - score: ", Points[player.score],
+		" - hasAdvantage: ", player.advantage,
+		" - isWinner: ", player.winner)
 }
