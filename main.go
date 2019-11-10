dbad6d34b3a3da79c1aff0e6a34448b446609e07
Jonathan Ivan Ferreira <jona.fer11@gmail.com>
2019-11-10 15:54:31 -0300

    Refactor scoring. Use Interface!! :D Is the best for the tests! :P

--- main.go
+++ main.go
@@ -17,6 +17,44 @@ type Player struct {
 	advantage bool
 }
 
+type PlayerInterface interface {
+	getName() string
+	getScore() int
+	hasAdvantage() bool
+	winScore()
+	setScore(score int)
+	setAdvantage(advantage bool)
+	setWinner(winner bool)
+}
+
+func (player *Player) getName() string {
+	return player.name
+}
+
+func (player *Player) getScore() int {
+	return player.score
+}
+
+func (player *Player) hasAdvantage() bool {
+	return player.advantage
+}
+
+func (player *Player) winScore() {
+	player.score++
+}
+
+func (player *Player) setScore(score int) {
+	player.score = score
+}
+
+func (player *Player) setAdvantage(advantage bool) {
+	player.advantage = advantage
+}
+
+func (player *Player) setWinner(winner bool) {
+	player.winner = winner
+}
+
 // Game is ....
 type Game struct {
 	player1 Player
@@ -27,26 +65,26 @@ func (game *Game) isThereWinner() bool {
 	return !(game.player1.winner == true || game.player2.winner == true)
 }
 
-func scoring(playerWin *Player, playerLose *Player) {
-	fmt.Println("Gool!!.. Point to", playerWin.name)
-	if playerWin.score < 2 {
-		playerWin.score++
-	} else if playerWin.advantage == false && playerLose.advantage == false {
-		if playerLose.score < 3 {
-			fmt.Println("Math Point to", playerWin.name)
+func scoring(playerWin PlayerInterface, playerLose PlayerInterface) {
+	fmt.Println("Gool!!.. Point to", playerWin.getName())
+	if playerWin.getScore() < 2 {
+		playerWin.winScore()
+	} else if playerWin.hasAdvantage() == false && playerLose.hasAdvantage() == false {
+		if playerLose.getScore() < 3 {
+			fmt.Println("Math Point to", playerWin.getName())
 		} else {
-			fmt.Println("Advantage to", playerWin.name, ".. Match Point!!!")
+			fmt.Println("Advantage to", playerWin.getName(), ".. Match Point!!!")
 		}
-		playerWin.score = 3
-		playerWin.advantage = true
-	} else if playerWin.advantage == true && playerLose.advantage == false {
-		fmt.Println("Winner is", playerWin.name)
-		playerWin.winner = true
-	} else if playerWin.advantage == false && playerLose.advantage == true {
+		playerWin.setScore(3)
+		playerWin.setAdvantage(true)
+	} else if playerWin.hasAdvantage() == true && playerLose.hasAdvantage() == false {
+		fmt.Println("Winner is", playerWin.getName())
+		playerWin.setWinner(true)
+	} else if playerWin.hasAdvantage() == false && playerLose.hasAdvantage() == true {
 		fmt.Println("Deuse!!!")
-		playerWin.score = 3
-		playerLose.score = 3
-		playerLose.advantage = false
+		playerWin.setScore(3)
+		playerLose.setScore(3)
+		playerLose.setAdvantage(false)
 	}
 }
 
@@ -55,6 +93,16 @@ func (game *Game) wonPoint() {
 		scoring(&game.player1, &game.player2)
 	} else {
 		scoring(&game.player2, &game.player1)
+	}
+}
+
+func (game *Game) playGame() {
+	continues := true
+	turn := 1
+	for continues {
+		turn = turn % 2
+		turn++
+		//game.player1
 	}
 }
 
