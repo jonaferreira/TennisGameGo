443dabd1611529e86589f637589c2b27db5a853d
Jonathan Ivan Ferreira <jona.fer11@gmail.com>
2019-11-10 22:37:00 -0300

    Add a test. But It needs to improve

--- main_test.go
+++ main_test.go
@@ -1,15 +1,24 @@
 package main
 
 type scoringResult struct {
-	player1 Player
-	player2 Player
+	playerIn1  Player
+	playerIn2  Player
+	playerOut1 Player
+	playerOut2 Player
 }
 
-/*var scoringResults = []scoringResult{
-	{Player{score: 0}, Player{score: 0},
-	{Player{score: 1}, Player{score: 0},
-}}
+var scoringResults = []scoringResult{
+	{Player{score: 0}, Player{score: 0}, Player{score: 1}, Player{score: 0}},
+	{Player{score: 1}, Player{score: 0}, Player{score: 2}, Player{score: 0}},
+	{Player{score: 2}, Player{score: 0}, Player{score: 3}, Player{score: 0}},
+	{Player{score: 3}, Player{score: 0}, Player{score: 4}, Player{score: 0}},
+	{Player{score: 3}, Player{score: 0}, Player{score: 3}, Player{score: 1}},
+	{Player{score: 3}, Player{score: 1}, Player{score: 3}, Player{score: 2}},
+	{Player{score: 3}, Player{score: 2}, Player{score: 3}, Player{score: 3}},
+	{Player{score: 3}, Player{score: 3}, Player{score: 3, advantage: true}, Player{score: 3, advantage: false}},
+}
 
+/*
 func TestScoring(t *testing.T) {
 
 }*/
