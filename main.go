3b620bc5578adccfb019407d9cdc1f675d9e8419
Jonathan Ivan Ferreira <jona.fer11@gmail.com>
2020-01-30 00:09:54 -0300

    Add log

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func (game *Game) playing() bool {
 		turn = turn % 2
 		turn++
 		if turn == 1 {
-			fmt.Println(game.player1.getName(), "is going to the ball... anndd...")
+			fmt.Println(game.player1.getName(), "is going to the ball... and...")
 			if rand.Float64() < 0.5 {
 				fmt.Println(game.player1.getName(), "can't return the ball")
 				scoring(&game.player2, &game.player1)
@@ -112,7 +112,7 @@ func (game *Game) playing() bool {
 				fmt.Println(game.player1.getName(), "had returned the ball")
 			}
 		} else {
-			fmt.Println(game.player2.getName(), "is going to the ball... anndd...")
+			fmt.Println(game.player2.getName(), "is going to the ball... and...")
 			if rand.Float64() < 0.5 {
 				fmt.Println(game.player2.getName(), "can't return the ball")
 				scoring(&game.player1, &game.player2)
